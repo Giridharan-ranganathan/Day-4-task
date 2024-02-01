492cf77173956787dc65d006a213954fce109b86
Giridharan-ranganathan <giridharan.ranganathan.99@gmail.com>
2024-02-01 18:12:12 +0530

    odd numbers in an array

---
+++ funtionTask.js
@@ -0,0 +1,18 @@
+// Print odd numbers in an array
+var odd = [];
+function oddFun(arr){
+for(var i=0;i<arr.length;i++){
+if(arr[i]%2 == 0){
+even.push(arr[i]);
+}
+else{
+odd.push(arr[i]);
+}
+}
+return odd;
+}
+console.log(oddFun([5,6,7,8,9,10,11,12]));
+
+// Convert all the strings to title caps in a string array
+
+
